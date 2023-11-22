b63013b798ed97ded658cb85e2b449215d0ce1aa
sahil143-debug <69601828+sahil143-debug@users.noreply.github.com>
2023-11-22 12:40:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+console.log("Welcome to Spotify");
+
+// Initialize the Variables
+
+let songIndex = 0;
+let audioElement = 0;
+let masterPlay = document.getElementById('masterPlay');
+let myProgressBar = document.getElementById('myProgressBar')
+
+let songs = [
+    {songName: "tu kaha hai", filePath: "songs/3.mp3", coverPath: "covers1.jpg"},
+    {songName: "Cielo - Huma-Huma", filePath: "songs/2.mp3", coverPath: "covers/2.jpg"},
+    {songName: "DEAF KEV - Invincible [NCS Release]-320k", filePath: "songs/3.mp3", coverPath: "covers/3.jpg"},
+    {songName: "Different Heaven & EH!DE - My Heart [NCS Release]", filePath: "songs/4.mp3", coverPath: "covers/4.jpg"},
+    {songName: "Janji-Heroes-Tonight-feat-Johnning-NCS-Release", filePath: "songs/5.mp3", coverPath: "covers/5.jpg"},
+    {songName: "Rabba - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/6.jpg"},
+    {songName: "Sakhiyaan - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/7.jpg"},
+    {songName: "Bhula Dena - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/8.jpg"},
+    {songName: "Tumhari Kasam - Salam-e-Ishq", filePath: "songs/2.mp3", coverPath: "covers/9.jpg"},
+    {songName: "Na Jaana - Salam-e-Ishq", filePath: "songs/4.mp3", coverPath: "covers/10.jpg"},
+]
+
+//master play 
+masterPlay.addEventListener('click', () => {
+    if (audioElement.paused || audioElement.currentTime <= 0) {
+        audioElement.play();
+    }
+})
+
+
+//listen to event
+myProgressBar.addEventListener('timeupdate'), function () {
+    console.log('timeupdate');
+}
+
+
